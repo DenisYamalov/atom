9eff0d5d82c7b65cf7652fab79826a4a14499f0e
DenisYamalov <nokiden91@gmail.com>
2020-06-30 16:56:47 +0300

    Started

--- lecture05/src/main/java/ru/atom/mm/MatchMakerApp.java
+++ lecture05/src/main/java/ru/atom/mm/MatchMakerApp.java
@@ -1,12 +1,16 @@
 package ru.atom.mm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.atom.mm.service.ConnectionQueue;
 
 
 @SpringBootApplication
 public class MatchMakerApp {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MatchMakerApp.class, args);
+        @Autowired
+        ConnectionQueue.size();
     }
 }
